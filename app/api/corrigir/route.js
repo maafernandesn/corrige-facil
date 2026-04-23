e84ace9feae1615f86b1265b0723bd77b1668851
maafernandesn <maafernandesn@gmail.com>
2026-04-23 17:56:21 -0300

    Atualizar o route.js

--- app/api/corrigir/route.js
+++ app/api/corrigir/route.js
@@ -52,7 +52,7 @@ Questão 1 - Alternativa: X
 Questão 2 - Alternativa: X`;
 
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
